df7005432458acfb5c827ca76fef69d7c9859c28
Francesco vera <francescoveraicomena@gmail.com>
2026-09-18 10:56:58 -0500

    he creado y editado 3 archivos js

---
+++ js/ejercicio_18.js
@@ -0,0 +1,12 @@
+const totalEstudiantes = 30;
+
+let mujeres = 18;
+let hombres = 12;
+
+const total = hombres + mujeres;
+let porcentajeMujeres = (Mujeres /total) * 100
+let porcentajeHombres = (Hombres /total) * 100
+
+console.log("Total de colaboradores: ", totalPersonas);
+console.log("Porcentaje de varones: ", porcentajeVarones.toFixed(2), "%");
+console.log("Porcentaje de mujeres: ", porcentajeMujeres.toFixed(2), "%");
