d455fb381ec9e87fb106d65f247a2c2b09b7b6f5
Francesco vera <francescoveraicomena@gmail.com>
2026-09-18 09:01:19 -0500

    he realizado el ejercico 05 de js

--- js/ejercicio_05.js
+++ js/ejercicio_05.js
@@ -1,1 +1,15 @@
-let 
+let nombreUsuario = "jose";
+let KilometrosRecorridos = 80;
+let CombustibleConsumido = 8;
+
+let RecorridoMotocicleta = KilometrosRecorridos * CombustibleConsumido;
+const cantidad = 0.10 * RecorridoMotocicleta;
+let CombustibleKilometro = RecorridoMotocicleta - cantidad;
+
+console.log("::::::::KILOMETROS RECORRIDOS::::::::");
+console.log("nombre del Usuario: ", nombreUsuario);
+console.log("Kilometros Recorridos: ", KilometrosRecorridos);
+console.log("Combustible Consumido: ", CombustibleConsumido);
+console.log("Recorrido Motocicleta: ", RecorridoMotocicleta);
+console.log("Cantidad: G",cantidad);
+console.log("combustible por Kilometro: ", CombustibleKilometro);
