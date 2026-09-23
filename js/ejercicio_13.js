264e1c217be92316bf2e4bc940a956553b479ba9
Francesco vera <francescoveraicomena@gmail.com>
2026-09-22 20:31:34 -0500

    estoy subiendo mis 3 archivos js

---
+++ js/ejercicio_13.js
@@ -0,0 +1,13 @@
+let numero = -18;
+
+if (
+    (numero >= -18 && numero <= 29) ||
+    (numero >= 90 && numero <= 105) ||
+    (numero >= 140 && numero <= 250)
+) {
+    if (numero > 0) {
+        console.log("El doble es: " + (numero * 2));
+    } else {
+        console.log("La mitad es: " + (numero / 2));
+    }
+} 
--- js/ejercicio_7.js
+++ js/ejercicio_7.js
@@ -1,1 +1,18 @@
-let
+let nota1 = 20
+let nota2 = 19
+let nota3 = 18
+
+let promedio = (nota1 + nota2 + nota3) / 3
+
+console.log("El promedio es: ", promedio)
+
+if (promedio > 10.5) {
+    console.log("Aprobado")
+
+    if (promedio > 18) {
+        console.log("Excelente")
+    }
+
+} else {
+    console.log("Desaprobado")
+}
